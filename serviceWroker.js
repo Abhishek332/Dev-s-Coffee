091c8aaa35174999640bff0461be3acad08ac92e
abhishek <abhishekp2k19@gmail.com>
2021-08-11 16:17:55 +0530

    Return cache

--- serviceWroker.js
+++ serviceWroker.js
@@ -18,7 +18,7 @@ const assets = [
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
+	return cache.addAll(assets)
     })
   )
 })
